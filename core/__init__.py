bea26ab94713a9eaeaef71eebd178e0d2873a6f2
VasilyevEvgeny <vasilev.evgeniy@physics.msu.ru>
2019-05-23 23:44:47 +0300

    readout xlsx and txt propagation logs

---
+++ core/__init__.py
@@ -0,0 +1,1 @@
+from .readout.xlsx_to_df import xlsx_to_df, txt_to_df
---
+++ core/readout/xlsx_to_df.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+def xlsx_to_df(path_to_xlsx, normalize_z_to=10**2, normalize_i_to=10**17):
+    df = pd.read_excel(path_to_xlsx)
+
+    df['z, m'] *= normalize_z_to
+    df['dz, m'] *= normalize_z_to
+    df['i_max, W / m^2'] /= normalize_i_to
+
+    df = df.rename(index=str, columns={'z, m': 'z_normalized', 'dz, m': 'dz_normalized', 'i_max, W / m^2': 'i_max_normalized'})
+
+    return df
+
+
+def txt_to_df(path_to_txt, normalize_z_to=10**2, normalize_i_to=10**17):
+    df = pd.read_csv(path_to_txt, sep='\t')
+
+    df['z[m]'] *= normalize_z_to
+    df['dz[m]'] *= normalize_z_to
+    df['I_max[W/m^2]'] /= normalize_i_to
+
+    df = df.rename(index=str,
+                   columns={'z[m]': 'z_normalized', 'dz[m]': 'dz_normalized', 'I_max[W/m^2]': 'i_max_normalized'})
+
+    return df
